a25486a2fe3959cf532c0127f0770e50bc07e832
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-06-01 20:25:04 -0400

    Time: 80 ms (5.58%), Space: 14.3 MB (7.85%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.py
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,15 +5,24 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
-        slow = fast = head
-        while fast.next and fast.next.next:
+#         slow = fast = head
+#         while fast.next and fast.next.next:
+#             slow = slow.next
+#             fast = fast.next.next
+        
+#         if fast.next:
+#             return slow.next
+#         else:
+#             return slow
+        
+        
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        if fast.next:
-            return slow.next
-        else:
-            return slow
+        return slow
         
         
         
