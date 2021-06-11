a48992a77a0735aa226549dbccb1420c43874ab5
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-06-10 23:10:37 -0400

    Time: 144 ms (11.49%), Space: 14.9 MB (6.55%) - LeetHub

--- find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,17 +1,42 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        left = 0
-        right = len(letters)-1
-        res = letters[0]
+        l = 0
+        r = len(letters)-1
         
-        while left<=right:
-            mid = left +(right-left)//2
-            if letters[mid] <= target:
-                left = mid +1
-            
+        while l<r:
+            mid = (l+r)//2
+            if letters[mid] > target:
+                r = mid
             else:
-                res = letters[mid]
-                right = mid -1
-        return res
-            
+                l = mid +1
+        return letters[l] if letters[l] > target else letters[0]
         
+#         l = 0
+#         r = len(letters)-1
+
+# 	    while l<r:
+#             mid = (l + (r-l))//2
+#             if letters[mid] > target:
+#                 r = mid
+#             else:
+#                 l = mid+1
+# 	    return letters[l] if letters[l] > target else letters[0]
+#         if letters[0]>target or letters[-1] == target:
+#             return letters[0]
+#         left = 0
+#         right = len(letters)-1
+#         res = letters[0]
+#         # if target == letters[right]:
+#         #     return res
+        
+#         while left <= right:
+#             mid = (right+left)//2
+#             if letters[mid] <= target:
+#                 left = mid +1
+#             else:
+#                 right = mid -1
+#         return res
+    
+        
+        
+                
