472aa0920ba39622e8a9b6f1437f4eeb035eb9ab
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-06-23 11:47:06 -0400

    Time: 996 ms (71.66%), Space: 25.2 MB (51.55%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # if len[prices] == 0:
-        #     return 0
+       
         if not prices:
             return 0
         profit = 0
