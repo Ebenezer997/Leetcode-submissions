98be813b888fe7027f7b4f63487f9c0fa85aa91a
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-07-05 21:03:01 -0400

    Time: 600 ms (81.51%), Space: 28.2 MB (58.53%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.py
+++ find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,13 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        #intialize the two pointers to the first element in the index
         turtle = hare = nums[0]
+        #turtle is a slow pointer moves one step at a time
         turtle = nums[turtle]
+        #hare is the fast pointer moves two steps at a time
         hare = nums[nums[hare]]
-        
+        #while turtle and hare has not meet. They will be moving in their pace
         while turtle != hare:
             turtle = nums[turtle]
             hare = nums[nums[hare]]
-        
+        #when they meet turtle will start again and both will move at the same pace.
         turtle = nums[0]
         while turtle != hare:
             turtle = nums[turtle]
