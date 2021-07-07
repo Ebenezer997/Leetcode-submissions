f7c02d2a8d6f231be83c0a78468ee30bc60c2bb8
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-07-07 11:59:41 -0400

    Time: 32 ms (92.23%), Space: 14.2 MB (83.88%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,25 +5,34 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        #if list 2 is empty return list 1 and inverse too
         if not l2 : return l1
         if not l1 : return l2
-        
+        # Assign dummy(temporay) head to  and also asign it to current.
         dummy = ListNode()
         current = dummy
         
+        #while list1 and list2 exist: if the current value in list1 is less than list 2 
+        #the next pointer is assigned to them
         while l1 and l2:
             if l1.val < l2.val:
+                #the next pointer is assigned to them
                 current.next = l1
+                #update the pointer to the next value in  list1
                 l1 = l1.next
+                #otherwise
             else:
                 current.next = l2
                 l2 = l2.next
+            #after comparing the current pointer is updated
             current = current.next
-        if l1:
-            current.next = l1
-        elif l2:
-            current.next = l2
+            current.next = l1 or l2
+        # if l1:
+        #     current.next = l1
+        # elif l2:
+        #     current.next = l2
         
+              
         return dummy.next
         
         
