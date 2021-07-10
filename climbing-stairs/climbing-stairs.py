1ad4227bd22fa4b638c61ed6c625dd4b64a81087
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-07-10 01:23:37 -0400

    Time: 28 ms (80.97%), Space: 14.2 MB (71.99%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,12 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <=2: return n
+        # 1,2,3,4,5
+        #steps:1,2,3,5,8,13
+        #according to the approach the steps looks at fibonacci sequence:
+        #The approach is to add successive steps.
+        
+        if n <= 2:return n
         prev1 = 1
         prev2 = 2
+        current = 0
         for i in range(2,n):
             current = prev1 + prev2
-            prev1= prev2
+            prev1 = prev2
             prev2 = current
+            
         return current
-       
+            
         
