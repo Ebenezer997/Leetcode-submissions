9cbcf9ea73dcf0804fc6cabe80cf6250ccc25c74
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-07-11 10:22:51 -0400

    Time: 44 ms (12.01%), Space: 14.3 MB (15.19%) - LeetHub

--- swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -5,11 +5,15 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        head2 = head
+        #have a temp head to hold the head
+        #While head and head.next exist we swap succcesvive elements
+        #after swapping you move the head to the next two positions and return the temp
+        
+        temp = head
         while head and head.next:
-            head.val, head.next.val = head.next.val, head.val
+            head.val,head.next.val = head.next.val,head.val
             head = head.next.next
             
-        return head2
+        return temp
     
         
