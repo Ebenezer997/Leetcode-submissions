be7a8848470f904a18a2312b853e842b4291a7d7
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-07-20 20:30:39 -0400

    Time: 240 ms (49.63%), Space: 15.4 MB (99.22%) - LeetHub

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,18 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums)-1
+        #two pointers one is the index of the first element(left) and index of the last element(right)
+        #We loop through and find the middle value: if the middle value is the target we return it if not 
+        #if the middle value is greater the tagert value we move left to the middle +1 index and continue our binary search else if it is less than the tagert we mmove right equal to middle-1. Till we find our element
+        N = len(nums)
+        l= 0
+        r = N-1
         
         while l<=r:
             mid = (l+r)//2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                l = mid +1
+            elif target > nums[mid]:
+                l = mid + 1
             else:
                 r = mid -1
+      
         return -1
-       
                 
                 
         
