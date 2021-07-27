0421e4d714289dc0db63fac652ab6f54094b9528
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-07-26 23:25:32 -0400

    Time: 72 ms (99.56%), Space: 15.4 MB (50.04%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.py
+++ merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
-        if not t1 and not t2:
-            return None
-        elif not t1 or not t2:
-            return t1 if t1 else t2
-
-        node = TreeNode(t1.val + t2.val)
-        node.left = self.mergeTrees(t1.left, t2.left)
+        if not t1 and not t2: return None
+        if not t1 : return t2
+        if not t2 : return t1
+        
+        node = TreeNode(t1.val+t2.val)
         node.right = self.mergeTrees(t1.right, t2.right)
+        node.left = self.mergeTrees(t1.left, t2.left)
+        
         return node
         
