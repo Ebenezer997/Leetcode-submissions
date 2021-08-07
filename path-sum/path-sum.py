ee1e9f9221422ad703eeb5ee2488eb44b4cb267e
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-08-07 16:30:37 -0400

    Time: 40 ms (82.97%), Space: 15.8 MB (96.03%) - LeetHub

--- path-sum/path-sum.py
+++ path-sum/path-sum.py
@@ -6,10 +6,15 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        #check if the tree is not empty
+        #you submit the values of the tree from the target sum at it traverses
+        #if the root reach the leef node and target sume is zero
+        # we does recursion for it to go again
+        
         if not root: return False
-        
         targetSum -= root.val
-        if root.left is None and root.right is None and targetSum == 0:
-            return True
-        return (self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum))
         
+        if not root.left and not root.right and targetSum == 0:
+            return True
+        
+        return (self.hasPathSum(root.left,targetSum)) or (self.hasPathSum(root.right,targetSum))
