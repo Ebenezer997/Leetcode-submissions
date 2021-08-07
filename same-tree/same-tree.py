b299f07b8e9018fb0507bbf36bab0cd0cd6c5f3c
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-08-07 15:47:11 -0400

    Time: 28 ms (83.17%), Space: 14.2 MB (85.86%) - LeetHub

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -6,13 +6,18 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+       #we think the a good appraoch will be bsf
+        #first we create a helper function and check if the two trees are the same while traversing.
+        #Return true if they are the same and return false if no
+        
+        
         def dfs(a,b):
             if not a and not b: return True
             if a and not b: return False
             if not a and b: return False
-            
             if a.val != b.val: return False
             
             return dfs(a.left,b.left) and dfs(a.right,b.right)
+        
         return dfs(p,q)
             
