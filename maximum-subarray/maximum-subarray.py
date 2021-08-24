44a86d0f7b8a322abdb1f7bf503da19392fc7fe8
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-08-24 00:50:59 -0400

    Time: 68 ms (47.92%), Space: 15 MB (60.70%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,8 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        total_sum = max_sum = nums[0]
+        #Use sliding window technique
+        # have two pointers and loop throu the array while adding
+        #Use MAx func to find the maximum sum while it loops
+        Total_sum = nums[0]
+        max_sum = nums[0]
         
         for i in (nums[1:]):
-            total_sum = max(i , total_sum+i)
-            max_sum = max(max_sum, total_sum)
+            Total_sum = max(i, Total_sum+i)
+            max_sum = max(Total_sum, max_sum)
         return max_sum
