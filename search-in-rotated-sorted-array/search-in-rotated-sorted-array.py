fa03729e750e969d858950dc9729a117af3c7003
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-09-02 00:44:38 -0400

    Time: 72 ms (5.00%), Space: 14.8 MB (23.05%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,30 +1,36 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums: return -1
-        
+        #using binary Search
+        #if nums(array) is empty return -1
+        if not nums :
+            return -1
+        #initializing pointers start and end
         start = 0
-        end = len(nums)-1
+        end = len(nums) - 1
         
+        # while the loop has not ended
         while start <= end:
-            mid = (start+end)//2
-            if target == nums[mid]:
+            #find mid
+            mid = (end + start)//2
+            #check if mid is the target and return it if it is.
+            if  target == nums[mid]:
                 return mid
-            
+            #if the value of nums[start] is less than nums[mid]
             if nums[start] <= nums[mid]:
-                if nums[start]<=target and target <nums[mid]:
-                    end = mid-1
+                #if target is between nums[start] and nums[mid]
+                if target >=nums[start] and target < nums[mid]:
+                    #update end pointer to  value before mid
+                    end = mid - 1
+                #else update the star pointer to the next value after mid
                 else:
-                    start = mid+1
+                    start = mid + 1
+            #if nums[start] > nums[end]
             else:
-                if target <= nums[end] and target > nums[mid]:
-                    start = mid+1
+                if target > nums[mid] and target <= nums[end]:
+                    start = mid + 1
                 else:
-                    end = mid-1
+                    end = mid - 1
         return -1
-                    
-                    
-                    
                 
-       
             
         
