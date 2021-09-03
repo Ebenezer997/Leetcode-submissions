b591e9636c51c76f42ce285d873a40cc2f4cd0a3
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-09-02 21:41:41 -0400

    Time: 60 ms (11.62%), Space: 14.7 MB (22.90%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,36 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #using binary Search
-        #if nums(array) is empty return -1
-        if not nums :
-            return -1
-        #initializing pointers start and end
-        start = 0
-        end = len(nums) - 1
+        if  not nums : return -1
         
-        # while the loop has not ended
+        start = 0
+        end = len(nums)-1
+        
         while start <= end:
-            #find mid
-            mid = (end + start)//2
-            #check if mid is the target and return it if it is.
-            if  target == nums[mid]:
+            mid = (start+end)//2
+            if target == nums[mid]:
                 return mid
-            #if the value of nums[start] is less than nums[mid]
             if nums[start] <= nums[mid]:
-                #if target is between nums[start] and nums[mid]
-                if target >=nums[start] and target < nums[mid]:
-                    #update end pointer to  value before mid
-                    end = mid - 1
-                #else update the star pointer to the next value after mid
+                if nums[start] <= target <nums[mid]:
+                    end = mid-1
                 else:
-                    start = mid + 1
-            #if nums[start] > nums[end]
+                    start = mid+1
             else:
-                if target > nums[mid] and target <= nums[end]:
-                    start = mid + 1
+                if nums[mid]<target<=nums[end]:
+                    start = mid+1
                 else:
-                    end = mid - 1
+                    end = mid-1
         return -1
-                
-            
+    
+        
         
