7b3494419faffb34a15905198a7b94ca3b4e70e1
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-09-04 11:30:51 -0400

    Time: 98 ms (24.26%), Space: 16.1 MB (54.47%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,23 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        #if the  list of intervals is less than 2 we return it
-        if len(intervals)< 2: return intervals
-        #sort the list of intervals for easily merging
+        if len(intervals)<2:
+            return intervals
         intervals.sort()
         
-        #initialize an output storage for intervals. it contains the first elements
         output = [intervals[0]]
         
-        #Loop thru array for start,end in intervals the second element
         for start,end in intervals[1:]:
-            #if the start of the current element is greater than the end of the prevous elements we append the current interval
             if start > output[-1][1]:
                 output.append([start,end])
-            #elif if the start of the current is not greater than the previous but the end is greater than the end of the previous that means it merges.
             elif end > output[-1][1]:
-                output[-1][1] = end
-        #return output
+                output[-1][1]= end
+        
         return output
-            
-            
-            
+        
