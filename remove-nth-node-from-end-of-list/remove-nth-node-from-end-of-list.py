cb36608e53ddddcf29489663e64be80b69c88861
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-09-07 23:10:26 -0400

    Time: 32 ms (78.77%), Space: 14 MB (91.86%) - LeetHub

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,17 +5,23 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        #fast and slow pointers
+        #two pointers one will go faster and reach the end and the other will follow after the other has reach  
         slow = head 
-        first = head
+        fast = head
+        #looping and first trip 
         for i in range(n):
-            first = first.next
-        
-        if not first:
+            fast = fast.next
+        # if fast does not exist we return the other value
+        if not fast:
             return head.next
-        
-        while first.next:
-            first = first.next
+        #second trip
+        while fast.next:
+            #move the fast pointer followed by slow pointer
+            fast = fast.next
             slow = slow.next
+            #when the fast pointer reach the end
+        #slow pointer is forwarded twice 
         slow.next = slow.next.next
-        
+        #return head
         return head
