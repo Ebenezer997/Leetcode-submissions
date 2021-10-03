6a8df21e565acfca423fb8d1ddade11630b027b1
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-03 10:48:59 -0400

    Time: 28 ms (83.31%), Space: 14 MB (98.27%) - LeetHub

--- first-bad-version/first-bad-version.py
+++ first-bad-version/first-bad-version.py
@@ -9,6 +9,9 @@ class Solution:
         :type n: int
         :rtype: int
         """
+        #Brute force approach is looping thru the version and returning the first version of bad version: this will be O(n): since we should minize te number of calls of the API i do not think this approach is best.
+        #Optimize Approach:
+        # use binary search to find the bad version.We start with te middle if it is a bad version we update the last pointer to the middle> Return Start
         end = n
         start = 0
         while start < end:
