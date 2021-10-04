7095dc5bd96feb7f5a7f28de758224ab052887c1
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-04 14:30:27 -0400

    Time: 385 ms (24.45%), Space: 25.6 MB (12.54%) - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
+        #since the array is a cycle; We find the modulo of k :
         
+        k %= len(nums)
+        #manipulation the array. we will cut where the end of the k is and add it to the beginning of the nums
         nums[:] = nums[len(nums)-k:] + nums[:len(nums)-k]
