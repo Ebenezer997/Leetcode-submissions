87f6c718a4055dacc102c5ee4932facc570dc250
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-23 16:01:58 -0400

    Time: 60 ms (83.47%), Space: 15.4 MB (20.27%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,21 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #create an empty list to the compliement
         h = {}
-        
-        #loop through the array
         for i in range(len(nums)):
-            
-            #calculate the compliment
             com = target - nums[i]
-            #check if the compliment is in the dictionary(h)
             if com in h:
-                #return it if it is in the dictionary
-                return (h[com],i)
-            # append this in dictionary
+                return( h[com], i)
             else:
-                h[nums[i]]= i
-            #big 0(n)
-        
-            
+                h[nums[i]] = i
         
