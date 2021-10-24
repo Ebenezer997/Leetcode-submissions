4454756e51e0287f1200e1c82b89638969e10371
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-23 23:30:29 -0400

    Time: 1076 ms (68.34%), Space: 25.2 MB (54.15%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #if array is empty return zero
         if not prices: return 0
         
-        #initializing profits and minimum price
         profit = 0
         mini = prices[0]
-        #looping through the array(list of prices)
+        
         for i in range(len(prices)):
-            #if the next price is lesser than the minimum 
             if prices[i] < mini:
-                #interchange it
                 mini = prices[i]
-            #else we compare the current profit with the previous profit
             else:
                 profit = max(profit,prices[i]-mini)
-        #return maximum profit when it ends
         return profit
-            
+        
+        
        
