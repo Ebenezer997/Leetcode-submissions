bb13afcb259f776b201c375f3bd3b6a974522e30
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-23 21:28:23 -0400

    Time: 164 ms (52.15%), Space: 15.3 MB (66.89%) - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -3,16 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # will take two pointer i and j.  initialize i = 0
-        # I will increase i if and only if I am swaping nums[j], nums[i]
-        #and I will swap nums[j],nums[i] when nums[j] is not zero
+        i = 0
         
-        #[0,1,0,3,12] -> intial nums[j] =0, nums[i]=0
-        # [0,1,0,3,12] -> intial nums[j] =1, nums[i] =0
-        # [1,0,0,3,12] -> swapped nums[j] and nums[i]
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
+                i += 1
         
-        i=0
-        for j in range (len(nums)):
-            if nums[j] !=0:
-                nums[i], nums[j]=nums[j],nums[i]
-                i+=1
+    
