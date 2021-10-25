31769990d0d84de717f45bfdbab40d9b02609ce8
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-24 23:11:24 -0400

    Time: 93 ms (11.69%), Space: 16.6 MB (34.48%) - LeetHub

--- average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
+++ average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -6,17 +6,7 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        #using bsf method and Big o(n)
-         #check if the tree is not empty:if empty you return none
-        # assign the  traverse to root
-        #create three empty lists: level:for value in each level , next_level : save values for next level, reults: save the final values.
-        #while the root exist you loop thru and add the nodes of each level to level
-        #you add the subtree of each level to the next_level
-        #you add the values the values in the level and divide it by the length
-        #assign the traverse to the next_level
-        #after that assign both level and next_level to empty list
-        
-        if not root:return None
+        if not root: return None
         
         traverse = [root]
         level = []
@@ -26,14 +16,15 @@ class Solution:
         while traverse:
             for node in traverse:
                 level.append(node.val)
-                
                 if node.left:
                     next_level.append(node.left)
                 if node.right:
-                    next_level.append(node.right)
+                     next_level.append(node.right)
             results.append(sum(level)/len(level))
             traverse = next_level
             next_level = []
             level = []
         return results
+                    
+                    
         
