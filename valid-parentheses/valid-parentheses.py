90e9b307eb495a90b492757c3824e3ef5bfd5ee3
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-26 00:01:45 -0400

    Time: 20 ms (99.47%), Space: 14.2 MB (64.18%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,14 +1,25 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        store = {")":"(","}":"{","]":"["}
+        #create a dictionary with matching pairs of parenthesis
+        store = {')':'(',']':'[','}':'{'}
+        
+        #create empty stack to contain strings
         stack = []
         
+        ##loop through S
         for i in s:
-            if i in store.values():
+            # if i not closing a parenthesis we add it to the stack
+            if i not in store.keys():
                 stack.append(i)
+            #if it is , we chack if stack not empty and check if they are pairs
             else:
-                if len(stack)>0 and store[i] == stack[-1]:
+                if len(stack)>0 and store[i] == stack[-1] :
+                    #they are pairs we pop it 
                     stack.pop()
+                #if are not we return false
                 else:
                     return False
+        #at the end we return ==[]
         return stack == []
+        
+        #big 0 is linear(0(n)) and space 0(n)
