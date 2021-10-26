0656eb3d531fe5c34771dec120830328c07df479
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-10-25 23:09:09 -0400

    Time: 82 ms (20.02%), Space: 15 MB (9.43%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,47 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        dicts = {}
+        s = sorted(s)
+        t = sorted(t)
         
-        for i in s:
-            if i in dicts:
-                dicts[i] += 1
-            else:
-                dicts[i] = 1
-        
-        for i in t:
-            if i in dicts:
-                dicts[i] -= 1
-            else:
-                return False
-        
-        for value in dicts.values():
-            if value != 0:
-                return False
-        
-        return True
-         
-        
-        
-        #for eg like s="a" & t="ab"
-        # if len(s) != len(t):
-            # return False
-	    #creating a set to cut the time of looping
-        # for i in set(s):
-		#counting whether the letters are repeted the same no of time
-            # if s.count(i) != t.count(i):
-            #     return False
-        # return True
-        
-        #big 0(n)
-#         if len(s) != len(t):
-#             return False
-#         elif sorted(s) == sorted(t):
-#             return True
-#         else:
-#             return False
-
-
-       
-        
-        
+        if s == t:
+            return True
+        else:
+            return False
