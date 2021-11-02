be98d54bbbc67404548b75177a616425e0072f0e
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-11-01 22:06:53 -0400

    Time: 68 ms (94.90%), Space: 14.5 MB (78.46%) - LeetHub

--- high-five/high-five.py
+++ high-five/high-five.py
@@ -1,5 +1,22 @@
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
+#         dic = {}
+        
+#         for item in items:
+#             if item[0] not in dic:
+#                 dic[item[0]] = [item[1]]
+#             else:
+#                 dic[item[0]].append([item[1]])
+        
+#         final = []
+        
+#         def avgsort(list):
+#             list = sorted(list, reverse = True)
+#             return int((sum(list[0:5]/5))
+        
+#         for item in dic:
+#             final.append([item,avgsort(dic[item])])
+#         return sorted(final)
         dic = {}
         for item in items:
             if item[0] in dic:
