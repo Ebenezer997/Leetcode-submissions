5cd5f11527fc6dba5c2d6c846c9f19454802f4d3
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2021-11-09 03:43:12 -0500

    Time: 44 ms (82.98%), Space: 14.5 MB (80.57%) - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-         #PU:: if you remove spaces, puntuaciations from a sentence and reverse it will be the same as the original sentece.
+               #PU:: if you remove spaces, puntuaciations from a sentence and reverse it will be the same as the original sentece.
         # create an empty string and loop thro the string .
         # you only consider only alphanumeric characters and also lower their case and add it to the empty string.
         #create two pointers one at the beginning index and other at the end. compare them every level if they ain't the same return false or after the end return true
@@ -21,3 +21,4 @@ class Solution:
                 return False
         return True
             
+        
