fe0d61fb441d81e69bd680af323253d3a9990a78
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2022-10-21 23:03:20 -0400

    Time: 257 ms (7.52%), Space: 14.2 MB (71.39%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -6,16 +6,16 @@ class Leaderboard:
     def addScore(self, playerId: int, score: int) -> None:
         self.board[playerId] += score
         
-
-    def top(self, K: int) -> int:
-        sumtop = 0
-        for player in self.board.most_common(K):
-            sumtop += player[1]
-        return sumtop
         
 
+    def top(self, K: int) -> int:
+        top = 0
+        for player in self.board.most_common(K):
+            top += player[1]
+        return top
+
     def reset(self, playerId: int) -> None:
-         self.board.pop(playerId)
+        self.board.pop(playerId)
         
 
 
