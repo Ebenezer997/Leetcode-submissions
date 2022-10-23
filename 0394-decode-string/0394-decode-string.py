5fa36bd68def3bb784fb28402475be8b30d9f524
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2022-10-23 17:13:44 -0400

    Time: 50 ms (63.43%), Space: 14 MB (19.72%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack= []
         
+        stack = []
         for i in range(len(s)):
-            if s[i]  != "]":
+            if s[i] != "]":
                 stack.append(s[i])
             else:
                 substr = ""
                 while stack[-1] != "[":
                     c = stack.pop()
                     substr = c + substr
-                #pop the closed bracket "["
                 stack.pop()
-                #find the number 
-                n = ""
+                n = ''
                 while stack and stack[-1].isdigit():
                     nums = stack.pop()
                     n = nums + n
                 stack.append(int(n)*substr)
         return "".join(stack)
-                
+                    
+                    
                 
         
