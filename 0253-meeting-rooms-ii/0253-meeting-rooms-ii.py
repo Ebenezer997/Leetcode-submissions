49d2f35c65441d4698d1a83c48f6b406a55e05b4
Ebenezer997 <58599920+Ebenezer997@users.noreply.github.com>
2022-10-25 14:31:06 -0400

    Time: 171 ms (44.74%), Space: 17.6 MB (30.37%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,23 +1,21 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals : return 0
-        used_rooms = 0
-        startlists = sorted([i[0] for i in intervals])
-        endlists = sorted([i[1] for i in intervals])
+        if not intervals: return 0
         
-        
+        startings = sorted([i[0] for i in intervals])
+        endings = sorted([i[1] for i in intervals])
         l = len(intervals)
-        sp = 0
+        used_rooms = 0
+        
+        sp= 0
         ep = 0
         
         while sp < l:
-            if startlists[sp] >= endlists[ep]:
+            if startings[sp] >=  endings[ep]:
                 used_rooms -= 1
                 ep += 1
-        
-        
+            
             used_rooms += 1
             sp += 1
-
+        
         return used_rooms
-                
